a96559c4030b84b3ad307dc7d83d94dc038b0e8b
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-22 17:10:29 +0600

    feat: add daisy ui, tailwind etc

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main data-theme="light">
+        <h2 className='btn'>khali bali</h2>
+      </main>
+    </>
+  )
+}
+
+export default App
