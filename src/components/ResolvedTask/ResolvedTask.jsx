73c0e4a565e9554d34f4b791815e795c20bffad8
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-22 23:57:27 +0600

    feat: add for showing task progress

---
+++ src/components/ResolvedTask/ResolvedTask.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResolvedTask = () => {
+    return (
+        <div className='border border-gray-200 shadow-xl p-3'>
+            <h3 className='text-[#34485A] text-2xl font-semibold mb-4'>Resolved Task</h3>
+            <p className='text-gray-500'>No resolved tasks yet.</p>
+        </div>
+    );
+};
+
+export default ResolvedTask;
---
+++ src/components/StatusTask/StatusTask.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StatusTask = () => {
+    return (
+        <div className='border border-gray-200 shadow-xl p-3'>
+            <h3 className='text-[#34485A] text-2xl font-semibold mb-4'>Task Status</h3>
+            <p className='text-gray-500'>Select a ticket to add to Task Status</p>
+        </div>
+    );
+};
+
+export default StatusTask;
