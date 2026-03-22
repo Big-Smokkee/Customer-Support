48288b03f058c100418e9f35d423cd4f5ea44bd7
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-22 20:54:14 +0600

    feat: add tickets fetching function for the promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { Suspense } from 'react'
 import './App.css'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
+import Tickets from './components/Tickets/Tickets'
+
+const ticketsFunction = async () => {
+  const res = await fetch('/tickets.json');
+  const json = res.json();
+  return json;
+}
+const ticketsPromise = ticketsFunction();
+
 
 function App() {
 
@@ -9,6 +19,12 @@ function App() {
       <main data-theme="light" className='inter'>
         <Navbar></Navbar>
         <Hero></Hero>
+        <div className='px-6 md:px-20'>
+          <Suspense fallback={<h4>loading</h4>
+          }>
+            <Tickets ticketsPromise={ticketsPromise}></Tickets>
+          </Suspense>
+        </div>
       </main>
     </>
   )
