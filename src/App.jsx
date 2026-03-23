e3daeff2ebc88fd3eac9c80713301ae7a8c9cf63
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-23 14:03:10 +0600

    add toast and function for handling the state for status of tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Tickets from './components/Tickets/Tickets'
 import StatusTask from './components/StatusTask/StatusTask'
 import ResolvedTask from './components/ResolvedTask/ResolvedTask'
-
+import { toast } from 'react-toastify';
 const ticketsFunction = async () => {
   const res = await fetch('/tickets.json');
   const json = res.json();
@@ -15,17 +15,32 @@ const ticketsPromise = ticketsFunction();
 
 
 function App() {
-
+  // hero state in-progress
+  const [progressTaskNumber, setProgressTaskNumber] = useState(0);
+  // ticket state progress or open
+  const [status, setStatus] = useState({});
+  const handleProgressTaskNumber = (ticket) => {
+    console.log(ticket);
+    setProgressTaskNumber(progressTaskNumber + 1);
+    toast('Task is In-Progress');
+  }
+  const handleStatus = (ticket) => {
+    console.log(ticket);
+    setStatus(prev => ({
+      ...prev,
+      [ticket.id]: "In-Progress"
+    }));
+  }
   return (
     <>
       <main data-theme="light" className='inter'>
         <Navbar></Navbar>
-        <Hero></Hero>
+        <Hero progressTaskNumber={progressTaskNumber}></Hero>
         <div className="px-6 md:px-20 flex flex-col md:flex-row gap-6">
           {/* Left side: tickets */}
           <div className="w-full md:w-4/5">
             <Suspense fallback={<h4>loading</h4>}>
-              <Tickets ticketsPromise={ticketsPromise}></Tickets>
+              <Tickets ticketsPromise={ticketsPromise} handleProgressTaskNumber={handleProgressTaskNumber} status={status} handleStatus={handleStatus}></Tickets>
             </Suspense>
           </div>
 
