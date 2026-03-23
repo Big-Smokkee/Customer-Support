b2979ff7e6e8e480cf7dfee71868a40e8de0ba4a
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-23 14:59:42 +0600

    feat: add responsiveness to the parent

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Tickets from './components/Tickets/Tickets'
-import StatusTask from './components/StatusTask/StatusTask'
+import StatusTasks from './components/StatusTasks/StatusTasks'
 import ResolvedTask from './components/ResolvedTask/ResolvedTask'
 import { toast } from 'react-toastify';
 const ticketsFunction = async () => {
@@ -16,16 +16,18 @@ const ticketsPromise = ticketsFunction();
 
 function App() {
   // hero state in-progress
-  const [progressTaskNumber, setProgressTaskNumber] = useState(0);
+  const [progressTaskNumber, setProgressTaskNumber] = useState([]);
   // ticket state progress or open
   const [status, setStatus] = useState({});
+
   const handleProgressTaskNumber = (ticket) => {
-    console.log(ticket);
-    setProgressTaskNumber(progressTaskNumber + 1);
+    const newTask = [...progressTaskNumber];
+    newTask.push(ticket);
+    setProgressTaskNumber(newTask);
     toast('Task is In-Progress');
   }
+
   const handleStatus = (ticket) => {
-    console.log(ticket);
     setStatus(prev => ({
       ...prev,
       [ticket.id]: "In-Progress"
@@ -38,15 +40,16 @@ function App() {
         <Hero progressTaskNumber={progressTaskNumber}></Hero>
         <div className="px-6 md:px-20 flex flex-col md:flex-row gap-6">
           {/* Left side: tickets */}
-          <div className="w-full md:w-4/5">
+          <div className="w-full md:w-4/6">
             <Suspense fallback={<h4>loading</h4>}>
               <Tickets ticketsPromise={ticketsPromise} handleProgressTaskNumber={handleProgressTaskNumber} status={status} handleStatus={handleStatus}></Tickets>
             </Suspense>
           </div>
 
           {/* Right side: status + resolved */}
-          <div className="w-full md:w-1/5 flex flex-col gap-6">
-            <StatusTask></StatusTask>
+          <div className="w-full md:w-2/6 flex flex-col gap-6">
+            {/* <StatusTasks progressTaskNumber={progressTaskNumber}></StatusTasks> */}
+            <StatusTasks progressTaskNumber={progressTaskNumber}> </StatusTasks>
             <ResolvedTask></ResolvedTask>
           </div>
         </div>
