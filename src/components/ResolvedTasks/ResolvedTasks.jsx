e4b887cdc1cff06d56755282f258eb4a0733999b
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-23 20:48:08 +0600

    feat: add functionality for not showing the resolved tickets in the customer tickets section

--- src/components/ResolvedTasks/ResolvedTasks.jsx
+++ src/components/ResolvedTasks/ResolvedTasks.jsx
@@ -5,9 +5,16 @@ const ResolvedTasks = ({ resolveTaskNumber }) => {
     return (
         <div className='border border-gray-200 shadow-xl p-3'>
             <h3 className='text-[#34485A] text-2xl font-semibold mb-4'>Resolved Task</h3>
-            <p className='text-gray-500'>No resolved tasks yet.</p>
+
             {
-                resolveTaskNumber.map(task => <ResolvedTask task={task}></ResolvedTask>)
+                resolveTaskNumber.length === 0 && <p className='text-gray-500'>No resolved tasks yet.</p>
+            }
+            {
+                resolveTaskNumber.length !== 0 && <div>
+                    {
+                        resolveTaskNumber.map(task => <ResolvedTask task={task} key={task.id}></ResolvedTask>)
+                    }
+                </div>
             }
         </div>
     );
--- src/components/Tickets/Tickets.jsx
+++ src/components/Tickets/Tickets.jsx
@@ -1,13 +1,19 @@
 import React, { use } from 'react';
 import Ticket from '../Ticket/Ticket';
 
-const Tickets = ({ ticketsPromise, handleProgressTaskNumber, status, handleStatus }) => {
-    const tickets = use(ticketsPromise)
+const Tickets = ({ ticketsPromise, handleProgressTaskNumber, status, handleStatus, resolveTaskNumber }) => {
+    const tickets = use(ticketsPromise);
+
+    const availableTickets = tickets.filter(ticket => {
+        const resolved = resolveTaskNumber.find(resolvedTicket => resolvedTicket.id === ticket.id);
+        return !resolved;
+    });
+
     return (
         <div>
             <h3 className="text-[#34485A] text-2xl font-semibold mb-4">Customer Tickets</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                {tickets.map(ticket =>
+                {availableTickets.map(ticket =>
                     <Ticket key={ticket.id} ticket={ticket} handleProgressTaskNumber={handleProgressTaskNumber} status={status[ticket.id] || ticket.status} handleStatus={handleStatus}></Ticket>
                 )}
             </div>
