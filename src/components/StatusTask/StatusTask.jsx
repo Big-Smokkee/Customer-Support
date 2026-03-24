35d8ac36a8ef695e926826e5e731f4c772c0948d
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-03-24 17:57:31 +0600

    feat: add footer section

--- src/components/StatusTask/StatusTask.jsx
+++ src/components/StatusTask/StatusTask.jsx
@@ -4,7 +4,7 @@ const StatusTask = ({ task, handleResolveTaskNumber }) => {
     return (
         <div className='border border-gray-200 p-4 rounded-xl my-2 shadow-lg space-y-2'>
             <h3 className='text-lg font-medium'>{task.title}</h3>
-            <button className="btn btn-active btn-success text-white font-semibold w-full" onClick={() => { handleResolveTaskNumber(task) }}>Success</button>
+            <button className="btn btn-active btn-success text-white font-semibold w-full" onClick={() => { handleResolveTaskNumber(task) }}>Complete</button>
         </div>
     );
 };
